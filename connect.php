85877f88a7466199f65032afaf22fd6b850f586e
Alex Manelis <amanelis@gmail.com>
2010-04-20 14:41:43 -0500

    xd_receiver.htm update

--- connect.php
+++ connect.php
@@ -14,17 +14,22 @@ $appapikey = 'YOURKEY';
 $appsecret = 'YOURSECRET'; 
 $facebook = new Facebook($appapikey, $appsecret);
 $fb_uid = $facebook->get_loggedin_user();
+
+//only change the exact text 'YOURKEY' with your key, you must leave on the _user
+//ex $_COOKIE['208345uhoergu082h4r-92h8_user;]
 $fb_user_cookie = $_COOKIE['YOURKEY_user'];
 
 //very important*******
-//try to grab facebook friends, if not then there is no user logged in
-//kill any user cookie by set_user(NULL)
+//try to grab facebook friends, if not then there is no user logged in, but cookie is still there
+//kill any user cookie by set_user(NULL), this is important, because of facebooks
+//cookies, you must set null if there was a recent login on browser.
 try {
     $friends = $facebook->api_client->friends_get();
 } catch (Exception $e) {
     $facebook->set_user(null, null);
 }
 
+//added cookie as redundancy check
 //if fb user is null or cookie is null show login button
 if(!$fb_uid || !$fb_user_cookie){
 ?>
@@ -47,43 +52,29 @@ if(!$fb_uid || !$fb_user_cookie){
 		$fb_crloc = $facebook->api_client->users_getStandardInfo($fb_uid,'current_location');
 		$fb_email = $facebook->api_client->users_getStandardInfo($fb_uid,'proxied_email');	
 		
-		//Parse any arrays, store values, prepare for auto form submission if new user
-		$f_lname = $fb_lname[0]['last_name'];
-		$f_fname = $fb_fname[0]['first_name'];
-		$f_wname = $fb_wname[0]['name'];
-		$f_uname = $fb_uname[0]['username'];
-		$f_local = $fb_local[0]['locale'];
-		$f_prurl = $fb_prurl[0]['profile_url'];
-		$f_brday = $fb_brday[0]['birthday'];
-		$f_sex	 = $fb_sex[0]['sex'];
-		$f_email = $fb_email[0]['proxied_email'];
-		$f_city  = $fb_crloc[0]['current_location']['city'];
-		$f_state = $fb_crloc[0]['current_location']['state'];
-		$f_crtny = $fb_crloc[0]['current_location']['country'];
-		$f_zipco = $fb_crloc[0]['current_location']['zip'];
-		
 		$f_today = date("Y-m-d");
 		$f_ip = $_SERVER['REMOTE_ADDR'];
 		
 		//This checks to see if this is first time fb users, you should have your db already connected at this point
-		$q_check_user = "SELECT fb_uid, school, logins, plogin, points FROM fbusers WHERE fb_uid = '$fb_uid'";
+		//Simply create a table with an 'fb_uid' field, if they are not in database, redirect them to a register page
+		//that adds them into database and stores their fb_uid.
+		$q_check_user = "SELECT fb_uid FROM fbusers WHERE fb_uid = '$fb_uid'";
 		$check_user_result = mysql_query($q_check_user);
 		$check_user_num_rows = mysql_num_rows($check_user_result);
 		mysql_free_result($check_user_result);		
 
 		//if no matching rows in query, NEW USER, grab values and send to a register form.
-		if($check_user_num_rows == 0 ){
+		if($check_user_num_rows == 0){
 			?>
 			<meta http-equiv="REFRESH" content="0;url=register.php?fb_uid=<? echo $fb_uid; ?>">
 			<?
 		//else, returning facebook user, show their information
 		}else {
-			echo "Welcome, <a href=\"http://www.facebook.com/profile.php?id=$fb_uid\">". $fb_fname[0]['first_name']." 
-			".$fb_lname[0]['last_name']."</a>.<br>";
+			echo "Welcome, <a href=\"http://www.facebook.com/profile.php?id=$fb_uid\">".$fb_fname[0]['first_name']." ".$fb_lname[0]['last_name']."</a>.<br>";
 			echo "You are signed in with your Facebook account.<br>";
 			?>
 			<br>
-			<a href="#" onClick="FB.Connect.logoutAndRedirect('/index.php')">Logout</a>
+			<a href="#" onClick="FB.Connect.logoutAndRedirect('/connect.php')">Logout</a>
 			<?
 		}
 	//if enters into this else statement, there is a serious problem.....
